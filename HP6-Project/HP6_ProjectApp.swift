d57614016e8b388008f6f373f8c71d7468f48f30
dobaophuc2008 <dobaophuc2008@gmail.com>
2025-06-29 11:40:09 +0700

    Initial Commit

---
+++ HP6-Project/HP6_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP6_ProjectApp.swift
+//  HP6-Project
+//
+//  Created by ho on 29/6/25.
+//
+
+import SwiftUI
+
+@main
+struct HP6_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
